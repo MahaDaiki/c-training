548dfc531ae7f0f93c55630b591f30f1e748126f
fatimamnaouer <fatimaezzahra.mnaouer@gmail.com>
2023-09-12 17:32:58 +0100

    md

--- main.c
+++ main.c
@@ -3,16 +3,15 @@
 
 int main(){
 
-<<<<<<< HEAD
+
 	printf("hello world everebody ");
 	float A, C, Y;
 	printf("entrer la valeur de A");
 	scanf("%f", &A);
 
-=======
+
 	printf("hello world my name is ayoub say welcom ");
 	printf("my name is mehdi");
->>>>>>> 442d078c84cc460599aac038f4ccb69316622dd0
 	return 0;
 }
 
