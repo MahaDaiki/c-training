3214c4ab873ebe3e864437f3912c27552bbf4d1c
MedHachami <71567163+MedHachami@users.noreply.github.com>
2023-09-18 19:00:46 +0200

    Delete me_Projects/toDoList directory

--- me_Projects/toDoList/deleteTask/deleteTask.h
+++
@@ -1,4 +0,0 @@
-#include<sqlite3.h>
-#include<stdio.h>
-
-extern int deleteTask(sqlite3*);
--- me_Projects/toDoList/main.h
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-
-#include "initDB/initDB.h"
-#include "addTask/addTask.h"
-#include "showTask/showTask.h"
-#include "editTask/editTask.h"
-#include "deleteTask/deleteTask.h"
